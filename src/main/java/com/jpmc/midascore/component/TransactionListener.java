5d0049e1b2570b3bf4c553c10e8802b5f4e55d36
niroopkris <niroopkris@gmail.com>
2025-08-30 15:13:56 -0700

    Finished task 1, working on task 2 kafka

---
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -0,0 +1,13 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionListener {
+    @KafkaListener(topics = "${kafka.topic.transaction}", groupId = "midas-core-group")
+    public void listen(Transaction transaction) {
+        System.out.println("Received transaction from " + transaction);
+    }
+}
